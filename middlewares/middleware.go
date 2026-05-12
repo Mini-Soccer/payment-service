0223bea4d74bac8cbd8b69478885dd6bf4f2078a
xylo <m.aditya3232@gmail.com>
2026-05-12 16:40:55 +0700

    add cors middleware

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -128,3 +128,16 @@ func Authenticate() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-service-name, x-request-at, x-api-key")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
